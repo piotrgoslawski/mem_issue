833be3cf463c66f298d9d6e8344d7422dc13123a
Piotr Goslawski <piotr@4point2.nl>
2022-08-23 11:41:06 +0200

    Track ctors and dump bin

--- main.cpp
+++ main.cpp
@@ -3,47 +3,98 @@
 #include <boost/asio/awaitable.hpp>
 
 
+struct StringWrapper {
+
+    StringWrapper(const std::string& s) : strField(s) {
+        std::cerr << "Create string " << reinterpret_cast<void*>(this) << std::endl;
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(this);
+    }
+
+    StringWrapper(const char* s) : strField(s) {
+        std::cerr << "Create string " << reinterpret_cast<void*>(this) << std::endl;
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(this);
+    }
+
+    StringWrapper(const StringWrapper& s) {
+        std::cerr << "Copy string from " << reinterpret_cast<const void*>(&s) << " to " << reinterpret_cast<void*>(this) << std::endl;
+        strField = s.strField;
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(&s);
+        BinaryPrint(this);
+    }
+
+    StringWrapper& operator=(const StringWrapper& s) {
+        std::cerr << "Copy string from " << reinterpret_cast<const void*>(&s) << " to " << reinterpret_cast<void*>(this) << std::endl;
+        strField = s.strField;
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(&s);
+        BinaryPrint(this);
+        return *this;
+    }
+
+    StringWrapper(StringWrapper&& s) {
+        std::cerr << "Move string from " << reinterpret_cast<const void*>(&s) << " to " << reinterpret_cast<void*>(this) << std::endl;
+        strField = std::move(s.strField);
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(&s);
+        BinaryPrint(this);
+    }
+
+    StringWrapper& operator=(StringWrapper&& s) {
+        std::cerr << "Move string from " << reinterpret_cast<const void*>(&s) << " to " << reinterpret_cast<void*>(this) << std::endl;
+        strField = std::move(s.strField);
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(&s);
+        BinaryPrint(this);
+        return *this;
+    }
+
+    ~StringWrapper() {
+        std::cerr << "Delete string " << reinterpret_cast<void*>(this) << std::endl;
+        std::cerr << "Data " << reinterpret_cast<void*>(strField.data()) << std::endl;
+        BinaryPrint(this);
+    }
+
+    void BinaryPrint(const StringWrapper* what) {
+
+        const char* c = reinterpret_cast<const char*>(what);
+
+        for(auto i = 0u; i < sizeof(StringWrapper); i++) {
+            std::cerr << std::hex << (int)*c++ << " ";
+        }
+        std::cerr << std::dec << std::endl;
+
+    };
+    std::string strField;
+};
+
 struct TestStruct {
-    std::string strField;
+    //explicit TestStruct(const std::string& str) : strField(str) {}
+    StringWrapper strField;
 };
-
-struct TestStructWithCtor {
-    explicit TestStructWithCtor(const std::string& str) : strField(str) {}
-    std::string strField;
-};
-
 
 boost::asio::awaitable<bool> TestFunction(TestStruct param) {
-    std::cout << param.strField << std::endl;
-    co_return true;
-}
-
-boost::asio::awaitable<bool> TestFunction2(TestStructWithCtor param) {
-    std::cout << param.strField << std::endl;
+    std::cout << param.strField.strField << std::endl;
     co_return true;
 }
 
 boost::asio::awaitable<void> TestCoroutine() {
-    auto testStruct = TestStruct{"some string"};
-
-    // This works
-    // co_await TestFunction(testStruct);
-    // co_await TestFunction2(TestStructWithCtor("some string"));
-    // co_await TestFunction2(TestStructWithCtor{.strField = "some string"});
-
-    // This causes mem issue
-    co_await TestFunction(TestStruct{"some string"});
+    // This does not work
+    co_await TestFunction(TestStruct{"    1    2"});
 }
 
 int main()
 {
-    boost::asio::io_context io_context;
+    boost::asio::thread_pool ctx(1);
 
     boost::asio::co_spawn(
-        io_context,
+        ctx,
         []() { return TestCoroutine(); },
-        boost::asio::detached);
+        boost::asio::use_future).wait();
 
-    io_context.run();
+    ctx.join();
+
     return 0;
 }
